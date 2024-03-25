8b09fa26063c6e1172c65eee8e9c85fe07342a31
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-03-24 20:49:35 -0400

    advertisement code optimize

--- assets/js/advertisementComponent.jsx
+++ assets/js/advertisementComponent.jsx
@@ -1,7 +1,17 @@
-function advertisementComponent() {
-    return (
-        <div className={"img-fluid row d-flex m-0 p-0 justify-content-center align-items-center"}>
-            <img src={"assets/img/adv1.webp"} className={"col-12 col-md-6 p-0"} alt={"Surprise 10000CAD Daily!"} />
-        </div>
-    );
+function advertisementComponent(data) {
+
+        return (
+            <div className={"img-fluid row d-flex m-0 p-0 justify-content-center align-items-center"}>
+                <img src={data.src} className={"col-12  p-0 w-100 h-auto"} alt={data.word} title={data.word} />
+            </div>
+        );
+
+
 }
+
+$(document).ready(function(){
+    let advertisement=$('#ADV-card-DOM')[0];
+    $.getJSON("assets/config/adv.json", function (data) {
+        ReactDOM.render(advertisementComponent(data), advertisement);
+    })
+});
