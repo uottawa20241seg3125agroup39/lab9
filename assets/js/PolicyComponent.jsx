fc03cea202efed7736830222ba81a82fbe26f239
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-03-25 21:09:03 -0400

    policy optimize

--- assets/js/PolicyComponent.jsx
+++ assets/js/PolicyComponent.jsx
@@ -1,26 +1,32 @@
-function RecentPolicyComponent() {
+
+function generatePolicyList(){
+    return new Promise((resolve, reject) => {
+        $.getJSON("assets/config/policy.json", function (data) {
+            resolve(
+                <ul>
+                    {data.map((item, index) => {
+                        return (
+                            <li className="item font-weight-normal text-primary text-sm-left"  key={index} data-id={index}>
+                                <a href={item.url} target="_blank">{item.name}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            )
+        })
+    })
+}
+
+
+
+async function RecentPolicyComponent() {
     return (
         <div className="right">
-            <ul>
-                <li className="item">
-                    <a href="./zytz_0/202401/t20240127_11234434.htm" target="_blank">Competition for the Chinese
-                        Government Scholarship 2024-2025（2024-01-26）</a>
-                </li>
-                <li className="item">
-                    <a href="./zytz_0/202312/t20231226_11212594.htm" target="_blank">Notice on the Abolition of Consular
-                        Authentication Services by the Chinese Embassy and Consulates-General in Canada after the
-                        Apost..（2023-12-25）</a>
-                </li>
-                <li className="item">
-                    <a href="./zytz_0/202312/t20231223_11210065.htm" target="_blank">Notice on the Chinese Public
-                        Holidays in 2024（2023-12-22）</a>
-                </li>
-                <li className="item">
-                    <a href="./zytz_0/202312/t20231220_11207314.htm" target="_blank">Notice（2023-12-19）</a>
-                </li>
-            </ul>
+            {await generatePolicyList()}
         </div>
     );
 }
 
-export default RecentPolicyComponent;
+$('document').ready(async function () {
+    ReactDOM.render(await RecentPolicyComponent(), $('#policy-card-DOM')[0]);
+})
